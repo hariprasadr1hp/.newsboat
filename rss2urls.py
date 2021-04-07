632c79367a20a66e51da85d99530df0d9be2522c
hariprasadr1hp <hariprasadr1hp@tutanota.com>
2021-04-07 21:26:26 +0200

    updated script

--- rss2urls.py
+++ rss2urls.py
@@ -1,79 +1,89 @@
-
+"""
+Convert a yaml file "rss.yml" containing RSS feeds to a newsboat
+supported "urls"
+"""
 import os
 import sys
 import yaml
 
 
-def loadRSSdata(yml_file):
-    with open(yml_file, 'r') as f:
-        content = yaml.load(f, yaml.FullLoader)
-    return content
+class urlsRSS:
+    """
+    Parameters
+    ----------
+    ymlfile : str
+        A yaml file containing the rss links and metadata
 
-def dump2URLS(urlfile, content):
-    with open(urlfile, 'w') as g:
-        newsfeeds   = content['newsfeeds']
-        youtube     = content['youtube']
-        reddit      = content['reddit']
-        # github      = content['github']
-        # gitlab      = content['gitlab']
-        # bitbucket   = content['bitbucket']
+    urlfile : str
+        A file to dump the contents
+    """
+    def __init__(self, ymlfile: str, urlfile: str) -> None:
+        self.ymlfile = ymlfile
+        self.urlfile = urlfile
+        self.loadRSS()
 
-        g.write("################################################\n")
-        g.write("# NEWSFEEDS\n")
-        g.write("################################################\n")
-        g.write("\n")
-        for link, title in newsfeeds.items():
-            g.write('{} "~FEED: {}"\n'.format(link,title))
-        g.write("\n")
-        g.write("################################################\n")
-        g.write("# YOUTUBE\n")
-        g.write("################################################\n")
-        g.write("\n")
-        for link, title in youtube.items():
-            g.write('https://www.youtube.com/feeds/videos.xml?channel_id={} "~YOUTUBE: {}"\n'.format(link,title))
-        g.write("\n")
-        g.write("################################################\n")
-        g.write("# REDDIT\n")
-        g.write("################################################\n")
-        g.write("\n")
-        for feed in reddit:
-            g.write('https://www.reddit.com/r/{}.rss "~REDDIT: {}"\n'.format(feed,feed))
-        g.write("\n")
-        g.write("################################################\n")
-        g.write("# GITHUB\n")
-        g.write("################################################\n")
-        g.write("\n")
-        # for feed in reddit:
-        #     g.write('https://www.reddit.com/r/{}.rss "~REDDIT: {}"\n'.format(feed,feed))
-        g.write("\n")
-        g.write("################################################\n")
-        g.write("# GITLAB\n")
-        g.write("################################################\n")
-        g.write("\n")
-        # for feed in reddit:
-        #     g.write('https://www.reddit.com/r/{}.rss "~REDDIT: {}"\n'.format(feed,feed))
-        g.write("\n")
-        g.write("################################################\n")
-        g.write("# BITBUCKET\n")
-        g.write("################################################\n")
-        g.write("\n")
-        # for feed in reddit:
-        #     g.write('https://www.reddit.com/r/{}.rss "~REDDIT: {}"\n'.format(feed,feed))
-        g.write("\n")
+    def loadRSS(self) -> None:
+        """
+        load the yaml file into a python dictionary
+        """
+        with open(self.ymlfile, 'r') as f:
+            self.content = yaml.load(f, yaml.FullLoader)
+
+    def dumpRSS(self) -> None:
+        """
+        Dumps the contents to a file
+        """
+        with open(self.urlfile, 'w') as g:
+            g.write("###############################################\n")
+            g.write("# RSS FEEDS\n")
+            g.write("###############################################\n\n")
+            for key, value in self.content.items():
+                if value['type'] == 'general':
+                    g.write('{} "~FEED: {}"\n'.format(key, value['name']))
+
+                elif value['type'] == 'youtube':
+                    line1 = "https://www.youtube.com/feeds/videos.xml?channel_id={}".format(key)
+                    line2 = '{} "~YOUTUBE: {}"\n'.format(line1, value['name'])
+                    g.write(line2)
+
+                elif value['type'] == 'reddit':
+                    line1 = "https://www.reddit.com/r/{}.rss".format(key)
+                    line2 = '{} "~REDDIT: {}"\n'.format(line1, value['name'])
+                    g.write(line2)
+
+                elif value['type'] == 'github':
+                    line1 = "https://www.github.com/{}.atom".format(key)
+                    line2 = '{} "~GITHUB: {}"\n'.format(line1, value['name'])
+                    g.write(line2)
+
+                elif value['type'] == 'gitlab':
+                    line1 = "https://www.gitlab.com/{}.atom".format(key)
+                    line2 = '{} "~GITLAB: {}"\n'.format(line1, value['name'])
+                    g.write(line2)
+
+                elif value['type'] == 'sourceforge':
+                    line1 = "https://www.sourceforge.net/p/{}/activity/feed".format(key)
+                    line2 = '{} "~SOURCEFORGE: {}"\n'.format(line1, value['name'])
+                    g.write(line2)
 
 
-def main(yml_file):
-    content = loadRSSdata(yml_file)
+
+def main():
+    """
+    main file
+    """
     urlfile = 'urls'
-    dump2URLS(urlfile, content)
+    a = urlsRSS(yml_file, urlfile)
+    a.dumpRSS()
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        yml_file = os.path.join(os.environ['HOME'],'.newsboat/rss.yml')
+        yml_file = "./rss.yml"
+        yml_file = os.path.join(os.environ['HOME'], ".newsboat/rss.yml")
     elif len(sys.argv) == 2:
-        yml_file = sys.argv[1]    
+        yml_file = sys.argv[1]
     else:
         print("Invalid number of arguments")
-        exit(1)
-    main(yml_file)
+        sys.exit(1)
+    main()
